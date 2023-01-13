818a37c815280a8ed9e92fb957d25037d1d753db
sumanta ghosh <sumantaghosh5333@gmail.com>
2023-01-13 13:05:00 +0530

    complete components

--- components/BigNFTSlider/BigNFTSlider.jsx
+++ components/BigNFTSlider/BigNFTSlider.jsx
@@ -82,15 +82,13 @@ const BigNFTSlider = () => {
           <div className={Style.bigNFTSlider_box_left_creator}>
             <div className={Style.bigNFTSlider_box_left_creator_profile}>
               <Image
+                className={Style.bigNFTSlider_box_left_creator_profile_img}
                 src={sliderData[idNumber].image}
-                alt="profile"
+                alt="profile image"
                 width={50}
                 height={50}
-                className={Style.bigNFTSlider_box_left_creator_profile_img}
               />
-              <div
-                className={Style.bigNFTSlider_box_left_creator_profile_image}
-              >
+              <div className={Style.bigNFTSlider_box_left_creator_profile_info}>
                 <p>Creator</p>
                 <h4>
                   {sliderData[idNumber].name}{" "}
@@ -116,14 +114,14 @@ const BigNFTSlider = () => {
             <div className={Style.bigNFTSlider_box_left_bidding_box}>
               <small>Current Bid</small>
               <p>
-                {sliderData[idNumber].price} <span>$2200</span>
+                {sliderData[idNumber].price} <span>$221,210</span>
               </p>
             </div>
             <p className={Style.bigNFTSlider_box_left_bidding_box_auction}>
               <MdTimer
                 className={Style.bigNFTSlider_box_left_bidding_box_icon}
               />
-              <span>Auction Ending In</span>
+              <span>Auction ending in</span>
             </p>
             <div className={Style.bigNFTSlider_box_left_bidding_box_timer}>
               <div
@@ -142,13 +140,13 @@ const BigNFTSlider = () => {
                 className={Style.bigNFTSlider_box_left_bidding_box_timer_item}
               >
                 <p>{sliderData[idNumber].time.minutes}</p>
-                <span>Minutes</span>
+                <span>mins</span>
               </div>
               <div
                 className={Style.bigNFTSlider_box_left_bidding_box_timer_item}
               >
                 <p>{sliderData[idNumber].time.seconds}</p>
-                <span>Seconds</span>
+                <span>secs</span>
               </div>
             </div>
             <div className={Style.bigNFTSlider_box_left_button}>
@@ -171,7 +169,7 @@ const BigNFTSlider = () => {
           <div className={Style.bigNFTSlider_box_right_box}>
             <Image
               src={sliderData[idNumber].nftImage}
-              alt="NFT Image"
+              alt="NFT IMAGE"
               className={Style.bigNFTSlider_box_right_box_img}
             />
             <div className={Style.bigNFTSlider_box_right_box_like}>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 import Style from "../styles/index.module.css";
-import {BigNFTSlider, HeroSection, Service} from "../components";
+import {
+  HeroSection,
+  Service,
+  BigNFTSlider,
+  Subscribe,
+  Title,
+  Category,
+  Filter,
+  NFTCard,
+  Collection,
+  AudioLive,
+  FollowerTab,
+  Slider,
+  Brand,
+  Video,
+} from "../components";
 
 const Home = () => {
   return (
@@ -7,6 +22,28 @@ const Home = () => {
       <HeroSection />
       <Service />
       <BigNFTSlider />
+      <Title
+        heading="Audio Collection"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <AudioLive />
+      <FollowerTab />
+      <Slider />
+      <Collection />
+      <Title
+        heading="Featured NFTs"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Filter />
+      <NFTCard />
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories."
+      />
+      <Category />
+      <Subscribe />
+      <Brand />
+      <Video />
     </div>
   );
 };
